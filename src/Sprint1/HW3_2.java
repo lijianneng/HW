b81adb2d1f1e818ddb50c5fbd25ce1cb5fbd2ea8
lijianneng <lijianneng1984@gmail.com>
2019-01-20 16:12:23 -0500

    test

--- src/Sprint1/HW3_2.java
+++ src/Sprint1/HW3_2.java
@@ -3,13 +3,15 @@ import java.util.Scanner;
 
 public class HW3_2 {
 	public static void main(String[] args) {
-		int number1 = (int)Math.random()*10;
-		int number2 = (int)Math.random()*10;
-		int number3 = (int)Math.random()*10;
+		int number1 = (int)(Math.random()*10);
+		int number2 = (int)(Math.random()*10);
+		int number3 = (int)(Math.random()*10);
 		
-		Scanner input = new Scanner(System.in);
 		System.out.print(
 			"What is " + number1 + " + " + number2 + " + " + number3 + "? ");
+		
+		Scanner input = new Scanner(System.in);
+		
 		int number = input.nextInt();
 		
 		
