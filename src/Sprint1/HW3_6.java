aa0222361ebd713643ab52ab3552dd04c8a5dd27
lijianneng <lijianneng1984@gmail.com>
2019-01-20 16:40:47 -0500

    test

---
+++ src/Sprint1/HW3_6.java
@@ -0,0 +1,40 @@
+package Sprint1;
+import java.util.Scanner;
+
+public class HW3_6 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter weight in pounds: ");
+		double weight = input.nextDouble();
+		
+		System.out.print("Enter feet: ");
+		double feet = input.nextDouble();
+		
+		System.out.print("Enter inches: ");
+		double inch = input.nextDouble();
+		
+		final double KILOGRAMS_PER_POUND = 0.45359237;
+		final double METERS_PER_FOOT = 0.3048;
+		final double METERS_PER_INCH = 0.0254;
+		
+		double weightInKilograms = weight * KILOGRAMS_PER_POUND;
+		double feetInMeters = feet * METERS_PER_FOOT;
+		double inchInMeters = inch * METERS_PER_INCH;
+		
+		double height = feetInMeters + inchInMeters;
+		
+		double bmi = weightInKilograms /
+				(height * height);
+		
+		System.out.println("BMI is " + bmi);
+		if (bmi < 18.5)
+		System.out.println("Underweight");
+		else if (bmi < 25)
+		System.out.println("Normal");
+		else if (bmi < 30)
+		System.out.println("Overweight");
+		else
+		System.out.println("Obese");
+	}
+
+}
