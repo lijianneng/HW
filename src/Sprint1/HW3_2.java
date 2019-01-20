858e54f6799ca9cecbde76e3376c3261230123e4
lijianneng <lijianneng1984@gmail.com>
2019-01-20 15:43:31 -0500

    test

--- src/Sprint1/HW3_2.java
+++ src/Sprint1/HW3_2.java
@@ -1,9 +1,21 @@
 package Sprint1;
+import java.util.Scanner;
 
 public class HW3_2 {
 	public static void main(String[] args) {
-		System.out.println("This is a test");
-		System.out.println("This is a test");
+		int number1 = (int)Math.random()*10;
+		int number2 = (int)Math.random()*10;
+		int number3 = (int)Math.random()*10;
+		
+		Scanner input = new Scanner(System.in);
+		System.out.print(
+			"What is " + number1 + " + " + number2 + " + " + number3 + "? ");
+		int number = input.nextInt();
+		
+		
+		System.out.println(number1 + " + " + number2 + " + " + number3 + " = " + number + " is " + 
+		(number1 + number2+ number3 == number));
+	
 
 	}
 }
